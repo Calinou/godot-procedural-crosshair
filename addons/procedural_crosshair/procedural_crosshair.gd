60c007ccce3f79f57d3dd12c9fc9f817150d41a6
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-07-09 23:29:47 +0200

    Implement shape rotation and marker arms
    This allows for the creation of T-shaped, caret or arrow markers.
    The T_SHAPE, CARET and ARROW marker types are now replaced by the LINE
    type which can handle all of them.

--- addons/procedural_crosshair/procedural_crosshair.gd
+++ addons/procedural_crosshair/procedural_crosshair.gd
@@ -8,10 +8,7 @@ extends Node2D
 
 enum Marker {
 	LINE,
-	T_SHAPE,
-	CARET,
 	TRIANGLE,
-	ARROW,
 	ARC,
 }
 
@@ -24,6 +21,9 @@ export(int, 3, 64) var shape_sides = 64
 
 # The width of the shape's stroke
 export(float, 0.0, 50.0) var shape_stroke_width = 1.1
+
+# The rotation of the shape (in degrees)
+export(float, -360.0, 360.0) var shape_rotation = 0.0
 
 # Whether to fill the shape with a solid color (see `shape_fill_modulate`)
 export var fill_shape = false
@@ -43,6 +43,14 @@ export(int, 0, 32) var markers_count = 4
 # The length of markers
 export(float, 0.0, 100.0) var marker_length = 10.0
 
+# The length of markers' arms
+# Non-zero values will create a T-shape or arrow depending on the arms' slope
+export(float, 0.0, 100.0) var marker_arms_length = 10.0
+
+# The slope of markers' arms
+# Higher values will look more "slanted"
+export(float, -100.0, 100.0) var marker_arms_slope = 0.0
+
 # The width of markers
 export(float, 0.0, 50.0) var marker_width = 2.0
 
@@ -51,11 +59,11 @@ export(float, 0.0, 50.0) var marker_width = 2.0
 # (negative value) of the markers
 export(float, -50.0, 50.0) var marker_offset = 0.0
 
-# The rotation of each marker
+# The rotation of each marker (in degrees)
 # TODO: Not yet implemented
 export(float, -360.0, 360.0) var marker_local_rotation = 0.0
 
-# The rotation of all markers
+# The rotation of all markers (in degrees)
 export(float, -360.0, 360.0) var marker_global_rotation = 0.0
 
 # The color to use at the base of markers
@@ -67,13 +75,6 @@ export(Color, RGBA) var marker_modulate_tip = Color(1.0, 1.0, 1.0, 0.0)
 # Whether to use anti-aliasing when available
 export var use_antialiasing = true
 
-var shape_points
-var shape_colors
-var marker_points
-var marker_colors
-var marker_point
-var marker_color
-
 func _spread_changed(spr):
 	spread = spr
 	update()
@@ -83,21 +84,17 @@ func _enter_tree():
 
 func _draw():
 	# Shape point array to be drawn
-	shape_points = []
+	var shape_points = []
 	# Shape color array to be drawn
-	shape_colors = []
+	var shape_colors = []
 	# Stores marker point arrays to be drawn
-	marker_points = []
+	var marker_points = []
 	# Stores marker color arrays to be drawn
-	marker_colors = []
-	# Array of points for a single marker
-	marker_point = []
-	# Array of colors for a single marker
-	marker_color = []
+	var marker_colors = []
 
 	# Shape
 	for point in range(0, shape_sides):
-		shape_points.append(Vector2(0, -spread).rotated(float(point)/shape_sides*TAU))
+		shape_points.append(Vector2(0, -spread).rotated(float(point)/shape_sides*TAU + deg2rad(shape_rotation)))
 
 	# Add the last point to close the shape
 	shape_points.append(shape_points[0])
@@ -106,24 +103,42 @@ func _draw():
 	for _point in shape_points:
 		shape_colors.append(shape_fill_modulate)
 
-	# Markers
-	for marker in range(0, markers_count):
-		# Line
-		marker_point.append([
-			Vector2(0, min(0, -marker_offset - spread)).rotated(float(marker)/markers_count*TAU + deg2rad(marker_global_rotation)),
-			Vector2(0, min(0, -marker_offset - spread - marker_length)).rotated(float(marker)/markers_count*TAU + deg2rad(marker_global_rotation)),
-		])
-
-		marker_color.append([
-			marker_modulate_base,
-			marker_modulate_tip,
-		])
-
-		marker_points.append(marker_point)
-		marker_colors.append(marker_color)
-
 	# Draw the shape's stroke
 	draw_polyline(shape_points, shape_stroke_modulate, shape_stroke_width, use_antialiasing)
+
+	# Markers
+	for marker in range(0, markers_count):
+		var rot = float(marker)/markers_count*TAU + deg2rad(marker_global_rotation)
+		if marker_style == Marker.LINE:
+			marker_points.append([
+				Vector2(0, min(0, -marker_offset - spread - marker_length)).rotated(rot),
+				Vector2(0, min(0, -marker_offset - spread)).rotated(rot),
+			])
+
+			marker_colors.append([
+				marker_modulate_base,
+				marker_modulate_tip,
+			])
+
+			marker_points.append([
+				Vector2(0, min(0, -marker_offset - spread - marker_length)).rotated(rot),
+				Vector2(-marker_arms_length, min(0, -marker_offset - spread - marker_length - marker_arms_slope)).rotated(rot),
+			])
+
+			marker_colors.append([
+				marker_modulate_base,
+				marker_modulate_tip,
+			])
+
+			marker_points.append([
+				Vector2(0, min(0, -marker_offset - spread - marker_length)).rotated(rot),
+				Vector2(marker_arms_length, min(0, -marker_offset - spread - marker_length - marker_arms_slope)).rotated(rot),
+			])
+
+			marker_colors.append([
+				marker_modulate_base,
+				marker_modulate_tip,
+			])
 
 	if fill_shape:
 		# Draw the shape's fill
@@ -131,4 +146,4 @@ func _draw():
 
 	# Draw markers
 	for index in range(0, marker_points.size()):
-		draw_polyline_colors(marker_point[index], marker_color[index], marker_width, use_antialiasing)
+		draw_polyline_colors(marker_points[index], marker_colors[index], marker_width, use_antialiasing)
